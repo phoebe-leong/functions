aae9f6e31d6094079cfbea28f121322530356d64
ph3b3 <phoebeleong8@gmail.com>
2021-08-21 13:52:18 +1000

    Create isnum.hpp

---
+++ isnum.hpp
@@ -0,0 +1,19 @@
+#include <string>
+
+bool isnum(std::string string) {
+    int amountofnumbers = 0;
+    char numbers[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+    for (int i = 0; i < string.size(); i++) {
+        for (int j = 0; j < sizeof(numbers) / sizeof(numbers[0]); j++) {
+            if (string[i] == numbers[j]) {
+                amountofnumbers++;
+                break;
+            }
+        }
+    }
+    if (amountofnumbers == string.size()) {
+        return true;
+    }
+    return false;
+}
