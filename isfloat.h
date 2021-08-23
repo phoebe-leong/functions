5792c436d47f5194e7c4c8eafab5b90f8f2c3f73
ph3b3 <phoebeleong8@gmail.com>
2021-08-23 11:55:52 +1000

    Create isfloat.h

---
+++ isfloat.h
@@ -0,0 +1,21 @@
+int isfloat(char string[]) {
+    int amountofnumbers = 0;
+    int amountofedecpoints = 0;
+    char numbers[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+    for (int i = 0; i < strlen(string); i++) {
+        for (int j = 0; j < sizeof(numbers) / sizeof(numbers[0]); j++) {
+            if (string[i] == numbers[j]) {
+                amountofnumbers++;
+                break;
+            } else if (string[i] == '.' && i != strlen(string) - 1) {
+                amountofedecpoints++;
+                break;
+            }
+        }
+    }
+    if (amountofedecpoints == 1 && amountofnumbers == strlen(string) - 1) {
+        return 0;
+    }
+    return 1;
+}
