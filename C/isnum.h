1e54c88fca05721d2e9d1b24a3181b7062780f50
ph3b3 <phoebeleong8@gmail.com>
2021-08-24 08:51:44 +1000

    added isposnum() and isminusnum()

--- C/isnum.h
+++ C/isnum.h
@@ -2,7 +2,7 @@
 #include <stdbool.h>
 #pragma once
 
-bool isnum(char string[]) {
+bool isposnum(char string[]) {
     int amountofnumbers = 0;
     char numbers[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
@@ -17,5 +17,28 @@ bool isnum(char string[]) {
     if (amountofnumbers == strlen(string)) {
         return true;
     }
-    return true;
+    return false;
+}
+
+bool isminusnum(char string[]) {
+    int amountofnumbers = 0;
+    char numbers[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+    for (int i = 0; i < strlen(string); i++) {
+        for (int j = 0; j < sizeof(numbers) / sizeof(numbers[0]); j++) {
+            if (string[i] == numbers[j]) {
+                amountofnumbers++;
+                break;
+            }
+        }
+    }
+    if (string[0] == '-' && amountofnumbers == strlen(string) - 1) {
+        return true;
+    }
+    return false;
+}
+
+bool isnum(char string[]) {
+    if (isminusnum(string) || isposnum(string)) return true;
+    return false;
 }
