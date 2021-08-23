c81af66e201a67837d0797d5648bc31bc004cf36
ph3b3 <phoebeleong8@gmail.com>
2021-08-23 11:55:13 +1000

    Create isfloat.hpp

---
+++ isfloat.hpp
@@ -0,0 +1,21 @@
+bool isfloat(std::string string) {
+    int amountofnumbers = 0;
+    int amountofdecpoints = 0;
+    char numbers[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+    for (int i = 0; i < string.size(); i++) {
+        for (int j = 0; j < sizeof(numbers) / sizeof(numbers[0]); j++) {
+            if (string[i] == numbers[j]) {
+                amountofnumbers++;
+                break;
+            } else if (string[i] == '.' && i != string.size() - 1) {
+                amountofdecpoints++;
+                break;
+            }
+        }
+    }
+    if (amountofdecpoints == 1 && amountofnumbers == string.size() - 1) {
+        return true;
+    }
+    return false;
+}
