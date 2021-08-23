3ef24cedcdfbdd6baa705116b93f09dcc51c3f0e
ph3b3 <phoebeleong8@gmail.com>
2021-08-24 08:50:02 +1000

    added isposnum() and isminusnum()

--- C++/isnum.hpp
+++ C++/isnum.hpp
@@ -1,7 +1,7 @@
 #include <string>
 #pragma once
 
-bool isnum(std::string string) {
+bool isposnum(std::string string) {
     int amountofnumbers = 0;
     char numbers[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
@@ -13,8 +13,31 @@ bool isnum(std::string string) {
             }
         }
     }
-    if (amountofnumbers == string.size() || amountofnumbers == string.size() - 1 && string[0] == '-') {
+    if (amountofnumbers == string.size()) {
         return true;
     }
+    return false;
+}
+
+bool isminusnum(std::string string) {
+    int amountofnumbers = 0;
+    char numbers[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+    for (int i = 0; i < string.size(); i++) {
+        for (int j = 0; j < sizeof(numbers) / sizeof(numbers[0]); j++) {
+            if (string[i] == numbers[j]) {
+                amountofnumbers++;
+                break;
+            }
+        }
+    }
+    if (string[0] == '-' && amountofnumbers == string.size() - 1) {
+        return true;
+    }
+    return false;
+}
+
+bool isnum(std::string string) {
+    if (isposnum(string) || isminusnum(string)) return true;
     return false;
 }
