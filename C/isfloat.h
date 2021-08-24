95bda6415d709fae228ef29ceccd2a84d44402c1
ph3b3 <phoebeleong8@gmail.com>
2021-08-24 18:13:18 +1000

    const string

--- C/isfloat.h
+++ C/isfloat.h
@@ -2,7 +2,7 @@
 #include <string.h>
 #pragma once
 
-bool isfloat(char string[]) {
+bool isfloat(const char string[]) {
     int amountofnumbers = 0;
     int amountofedecpoints = 0;
     char numbers[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
