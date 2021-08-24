c887077c3694af2207248fc4e9fb0f509578eacb
ph3b3 <phoebeleong8@gmail.com>
2021-08-24 18:13:47 +1000

    constant

--- C++/isfloat.hpp
+++ C++/isfloat.hpp
@@ -1,7 +1,7 @@
 #include <string>
 #pragma once
 
-bool isfloat(std::string string) {
+bool isfloat(const std::string string) {
     int amountofnumbers = 0;
     int amountofdecpoints = 0;
     char numbers[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
